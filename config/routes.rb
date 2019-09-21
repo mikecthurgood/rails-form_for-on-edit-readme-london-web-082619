3768cf7cbf3eca4cec2d0fff18d3c4b00b8d86c2
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-21 16:52:10 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/posts", to: "posts#index", as: "posts"
+  get "/posts/new", to: "posts#new", as: "new_post"
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  post "/posts", to: "posts#create"
+  patch "/posts/:id", to: "posts#update"
 end
